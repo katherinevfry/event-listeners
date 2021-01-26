dd8fe1dd38bce9b673f1076e0f80eddd802be54f
Katy-fry <katyvfry@gmail.com>
2021-01-25 19:47:25 -0600

    set up html with divs and buttons

--- main.js
+++ main.js
@@ -1,64 +1,62 @@
-const firstObj = {};
-
-const user = {
-username: 'biz.markie',
-password: 'abcd',
-lovesJavascript: true,
-favoriteNumber: 42,
-};
-
-// DOT NOTATION
-
-console.log(user.lovesJavascript); // will log true to the console
-console.log(user.username); //expect bizmarkie
-
-// BRACKET NOTATION
-// console.log(user['password']); //must be a strong, number, or variable
-
-// console.log(user['favoriteNumber']);
-
-// console.log(user.favoriteNumber);
-
-// ASSIGNING VALUES
-
-const newUser = {
-  isNew: true,
-}
-
-// newUser.username = 'fresh.prince'; //asignment via dot notation
-// newUser['password'] = 'abcdsdfsa'; //asignment via bracket notation
-// console.log(newUser);
-
-//METHODS
-// you can assign functions as values of an object. they're called methods
-
-const myObject = {
-  username: 'Katy',
-  sayHello: function () {
-    console.log(`${this.username} says hello`) //this = this object. will console log Katy says hello
+const pies = [
+  {
+    name: 'Dutch Apple Pie',
+    ingredients: 'apples,sugar,butter,nutmeg,dutch people',
+    bakeTemp: 5000,
+    drinkPairing: 'Earl Grey Tea',
+    imageUrl:
+      'https://images-gmi-pmc.edge-generalmills.com/b6a2a4e7-73f5-4aec-9bb6-f2b5054d65e6.jpg',
+    instructor: 'Doc',
+    iceCream: 'Vanilla',
   },
-}
-//anonymous functions don't have names
-// myObject.username = 'Katherine'; //will now log Katherine says hello
-// myObject.sayHello() //you still have to invoke the function with parentheses
-
-//ITERATE OVER AN OBJECT
-
-const userTwo = {
-  username: 'Trinity',
-  password: 'xyz0987',
-  lovesJavascript: true,
-  favoriteNumber: 12,
-}
-
-//time to use a for...in loop
-
-for (let key in userTwo) {
-  console.log(key); //logs keys
-} 
-
-for (let key in userTwo) {
-  console.log(userTwo[key]); //logs value
-}
-
-//you have to use bracket, NOT dot notation
+  {
+    name: 'Berry Pie',
+    ingredients: 'berries',
+    bakeTemp: 400,
+    drinkPairing: 'wine',
+    imageUrl:
+      'https://tastesbetterfromscratch.com/wp-content/uploads/2015/11/Triple_Berry_Pie8.jpg',
+    instructor: 'Doc',
+    iceCream: 'banana',
+  },
+  {
+    name: 'Pumpkin Pie',
+    ingredients: 'pumpkins, nutmeg, cinnamon, graham crackers, pilgrims',
+    bakeTemp: 42,
+    drinkPairing: 'egg nog',
+    imageUrl:
+      'https://cf-images.us-east-1.prod.boltdns.net/v1/static/1033249144001/22a0c25d-2fee-415c-a4e7-91d008e276bb/a904f3e2-3cd9-40d6-ace9-f8dbd2d616cd/1280x720/match/image.jpg',
+    instructor: 'Aja',
+    iceCream: 'Vanilla',
+  },
+  {
+    name: 'Shoo Fly Pie',
+    ingredients: 'Molasses, nutmeg, cinnamon, butter, graham cracker ',
+    bakeTemp: 1234,
+    drinkPairing: 'Apple Cider',
+    imageUrl:
+      'https://static01.nyt.com/images/2016/09/27/dining/27COOKING-SHOOFLY-PIE2/27COOKING-SHOOFLY-PIE2-articleLarge.jpg',
+    instructor: 'Aja',
+    iceCream: 'Coffee',
+  },
+  {
+    name: 'Pecan Pie',
+    ingredients: 'Pecans, sugar, butter, flour',
+    bakeTemp: 5000,
+    drinkPairing: 'Milk',
+    imageUrl:
+      'https://cookiesandcups.com/wp-content/uploads/2018/10/pecanpie-3.jpg',
+    instructor: 'Trinity',
+    iceCream: 'Vanilla',
+  },
+  {
+    name: 'Keylime Pie',
+    ingredients: 'lemons, sugar, butter, flour',
+    bakeTemp: 5000,
+    drinkPairing: 'Water',
+    imageUrl:
+      'https://www.browneyedbaker.com/wp-content/uploads/2012/05/key-lime-pie-2-1200.jpg',
+    instructor: 'Trinity',
+    iceCream: 'none',
+  },
+];
